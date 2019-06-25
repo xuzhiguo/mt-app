fb4ed0146df8468e135b08233a5878128050ac59
xuzhiguo <1127391449@qq.com>
2019-06-25 09:48:02 +0800

    构建 产品页相关插件

---
+++ pages/products.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-row class="page-product">
+    <el-col :span="19"></el-col>
+    <el-col :span="5"></el-col>
+    <crumbs />
+  </el-row>
+</template>
+
+<script>
+import Crumbs from '../components/products/crumbs'
+import Categroy from '../components/products/categroy'
+import List from '../components/products/list'
+import Iselect from '../components/products/iselect'
+
+export default {
+  components: {
+    Crumbs,
+    Categroy,
+    List,
+    Iselect
+  },
+  async asyncData(ctx) {
+    
+  },
+}
+</script>
+
+<style lang="scss">
+  @import '@/assets/css/products/index.scss'
+</style>
